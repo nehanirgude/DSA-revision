7c84ab07c775bdcd22e733db90c8f51b2d41c95f
nehanirgude <nehanirgude3@gmail.com>
2024-12-24 16:32:41 +0530

    done

--- OOPs.java/Constructorss.java
+++ OOPs.java/Constructorss.java
@@ -1,8 +1,12 @@
 public class Constructorss {
 
     public static void main(String args[]) {
+        // java make default constructor only if there is not a single constructor is
+        // made
 
         Student s1 = new Student(); // contructor is called
+
+        Student s2 = new Student("neha"); // constructor call for parameterized constructor
 
     }
 }
@@ -15,6 +19,12 @@ class Student {
     // if we dont make any contructor still java make a default custructor
     Student() {
         System.out.println("constructer is called");
-    }
 
+    }
+    // parameterized constructor
+
+    Student(String name) { // parameter is passed during defination is called parameterized constructor
+
+        this.name = name;
+    }
 }

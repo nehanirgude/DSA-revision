c1f2b3e7a8c3a931e8125331de2bfe680416acec
nehanirgude <nehanirgude3@gmail.com>
2024-12-29 18:31:09 +0530

    done

--- Linkedlist/middle.java
+++ Linkedlist/middle.java
@@ -159,6 +159,52 @@ public class middle {
         return helper(head, key);
     }
 
+    // code to reverse a linkedlist
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    // program to nth node from end
+    public void deleteNthfromEnd(int n) {
+        // calculate size
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+
+        }
+        if (n == sz) {
+            head = head.next; // removeFirst
+            return;
+
+        }
+
+        // sz-n
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+
+        }
+        prev.next = prev.next.next;
+        return;
+
+    }
+
     public static void main(String args[]) {
         // obj creation
         middle ll = new middle();

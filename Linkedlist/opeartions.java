73b6095e7096081ae7920028e3876a18e98bac70
nehanirgude <nehanirgude3@gmail.com>
2024-12-30 16:52:01 +0530

    done

--- Linkedlist/opeartions.java
+++ Linkedlist/opeartions.java
@@ -269,13 +269,45 @@ public class opeartions {
         return false; // cycle doesn't exist
     }
 
+    // remove cycle from linkedlist code
+    public static void removeCycle() {
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+        // find meeting point
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // remove cycle-> last.next=null
+        prev.next = null;
+    }
+
     public static void main(String args[]) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
         head.next.next.next = head;
         // loop formed as 1->1->2->3->1
 
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
 
     }

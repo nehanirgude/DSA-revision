94d1a84bd1149df49a3a734b757a1c0c615acbd9
nehanirgude <nehanirgude3@gmail.com>
2024-12-31 20:09:13 +0530

    done

--- Linkedlist/opeartions.java
+++ Linkedlist/opeartions.java
@@ -1,5 +1,7 @@
 package Linkedlist;
 
+import java.util.LinkedList;
+
 public class opeartions {
     public static class Node {
 
@@ -297,18 +299,94 @@ public class opeartions {
         // remove cycle-> last.next=null
         prev.next = null;
     }
+    // merge sort code
+
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return slow; // mid node
+
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // find mid
+        Node mid = getMid(head);
+        // left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        // merge
+        return merge(newLeft, newRight);
+    }
 
     public static void main(String args[]) {
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = head;
-        // loop formed as 1->1->2->3->1
+        // head = new Node(1);
+        // Node temp = new Node(2);
+        // head.next = temp;
+        // head.next.next = new Node(3);
+        // head.next.next.next = head;
+        // // loop formed as 1->2->3->2
 
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+        // System.out.println(isCycle());
+        // removeCycle();
+        // System.out.println(isCycle());
+
+        opeartions ll = new opeartions();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        // 5->4->3->2->1
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
 
     }
 

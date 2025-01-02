d9a29a8b3468193172fb36526cb49e510926af64
nehanirgude <nehanirgude3@gmail.com>
2025-01-02 11:47:28 +0530

    done

--- stack/PushAtBottom.java
+++ stack/PushAtBottom.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class PushAtBottom {
 
-    // public static void pushAtBottom(Stack<Integer> s, int data) {
-    // if (s.isEmpty()) {
-    // s.push(data);
-    // return;
-    // }
-    // int top = s.pop();
-    // pushAtBottom(s, data);
-    // s.push(top);
-    // }
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
 
     // function to reverse a string using stack
 

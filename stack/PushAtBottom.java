22274ad1071ecca6dd78777b0baf9d70fc9503e2
nehanirgude <nehanirgude3@gmail.com>
2025-01-02 11:24:04 +0530

    done program for reverse a string

--- stack/PushAtBottom.java
+++ stack/PushAtBottom.java
@@ -1,28 +1,49 @@
 package stack;
 
-import java.util.Stack;
+import java.util.*;
 
 public class PushAtBottom {
 
-    public static void pushAtBottom(Stack<Integer> s, int data) {
-        if (s.isEmpty()) {
-            s.push(data);
-            return;
+    // public static void pushAtBottom(Stack<Integer> s, int data) {
+    // if (s.isEmpty()) {
+    // s.push(data);
+    // return;
+    // }
+    // int top = s.pop();
+    // pushAtBottom(s, data);
+    // s.push(top);
+    // }
+
+    // function to reverse a string using stack
+
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
         }
-        int top = s.pop();
-        pushAtBottom(s, data);
-        s.push(top);
+        StringBuilder result = new StringBuilder(" ");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
     }
 
     public static void main(String args[]) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        pushAtBottom(s, 4);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+        // // pushAtBottom(s, 4);
+        // while (!s.isEmpty()) {
+        // System.out.println(s.pop());
+        // }
+
+        String str = "abc";
+        String result = reverseString(str);
+        System.out.println(result);
 
     }
 
